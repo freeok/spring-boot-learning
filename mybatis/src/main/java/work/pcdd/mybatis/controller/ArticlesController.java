09842064d90cf8466ca530a16f51b74df168b5b3
freeok <51998152+freeok@users.noreply.github.com>
2024-03-12 20:55:51 +0800

    mybatis模块: 新增一对多用法

--- mybatis/src/main/java/work/pcdd/mybatis/controller/ArticlesController.java
+++ mybatis/src/main/java/work/pcdd/mybatis/controller/ArticlesController.java
@@ -1,18 +1,36 @@
 package work.pcdd.mybatis.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import work.pcdd.mybatis.entity.Authors;
+import work.pcdd.mybatis.mapper.ArticlesMapper;
+
+import java.util.List;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author pcdd
  * @since 2024-03-12
  */
-@Controller
+@RestController
 @RequestMapping("/articles")
 public class ArticlesController {
+
+    @Autowired
+    ArticlesMapper articlesMapper;
+
+    /**
+     * 传入 id 表示单查，否则表示全查
+     */
+    @GetMapping
+    public List<Authors> findById(@RequestParam(required = false) Integer id) {
+        return articlesMapper.find(id);
+    }
 
 }
--- mybatis/src/main/java/work/pcdd/mybatis/mapper/ArticlesMapper.java
+++ mybatis/src/main/java/work/pcdd/mybatis/mapper/ArticlesMapper.java
@@ -1,13 +1,19 @@
 package work.pcdd.mybatis.mapper;
 
+import work.pcdd.mybatis.entity.Authors;
+
+import java.util.List;
+
 /**
  * <p>
- *  Mapper 接口
+ * Mapper 接口
  * </p>
  *
  * @author pcdd
  * @since 2024-03-12
  */
 public interface ArticlesMapper {
+
+    List<Authors> find(Integer id);
 
 }
