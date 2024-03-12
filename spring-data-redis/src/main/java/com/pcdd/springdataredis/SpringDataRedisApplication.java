34927cb623446d3b8a7aea2ee14d42eee2422e00
pcdd <akapcdd@163.com>
2024-03-13 03:23:30 +0800

    :tada: spring-data-redis模块初始化

---
+++ spring-data-redis/src/main/java/com/pcdd/springdataredis/SpringDataRedisApplication.java
@@ -0,0 +1,16 @@
+package com.pcdd.springdataredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author pcdd
+ */
+@SpringBootApplication
+public class SpringDataRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataRedisApplication.class);
+    }
+
+}
